a071fdee3938db747b47d1dfa75f75c94baec98b
Gordon Su <gordonsu14@gmail.com>
2016-10-07 01:13:20 -0400

    External Interrupts with button and 4 bit counter

--- src/main.c
+++ src/main.c
@@ -3,47 +3,71 @@
 #include <stdint.h>
 #include <math.h>
 #include "stm32f4xx_conf.h"
-#include "utils.h"
+
+void INTTIM_Config();
+TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 
 // Private variables
-volatile uint32_t time_var1, time_var2;
+volatile uint32_t time_var1, time_var2, debounce;
 
-// Private function prototypes
-void Delay(volatile uint32_t nCount);
 void init();
-void calculation_test();
-void dac_test();
+void Delay(volatile uint32_t nCount);
+void incrementCounter();
+void EXTI0_IRQHandler(void);
+void Configure_PA0(void);
 
 int main(void) {
 	init();
 
-	calculation_test();
-	dac_test();
+	/*int LED_MASK = 0b1111 << 12;*/
+	/*while(1) {*/
+		/*GPIOD->ODR += (1 << 12);*/
+		/*if(GPIOD->ODR & LED_MASK == 0b1111) */
+			/*GPIOD->ODR ^= 0b1111 << 12;*/
+		/*Delay(500);*/
+	/*}*/
+
 
 	for(;;) {
-
 	}
 
 	return 0;
 }
 
-void dac_test() {
-	for(;;) {
-		for (float i = 0;i < 2.0 * M_PI;i += (2.0 * M_PI) / 100.0) {
-			DAC_SetChannel1Data(DAC_Align_12b_R, (uint32_t)((sinf(i) + 1.0) * 2047.0));
-		}
-	}
+/*
+ * Delay a number of systick cycles (1ms)
+ */
+void Delay(volatile uint32_t nCount) {
+	time_var1 = nCount;
+	while(time_var1){};
 }
 
+/*
 void calculation_test() {
 	float a = 1.001;
 	int iteration = 0;
 
 	for(;;) {
-		GPIO_SetBits(GPIOD, GPIO_Pin_12);
-		Delay(500);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
-		Delay(500);
+		int value = iteration % 4;
+		switch (value) {
+			case 0:
+				GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+				GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+				break;
+			case 1:
+				GPIO_SetBits(GPIOD, GPIO_Pin_12);
+				GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+				break;
+			case 2:
+				GPIO_SetBits(GPIOD, GPIO_Pin_14);
+				GPIO_ResetBits(GPIOD, GPIO_Pin_12); break;
+			case 3:
+				GPIO_SetBits(GPIOD, GPIO_Pin_14);
+				GPIO_SetBits(GPIOD, GPIO_Pin_12);
+				break;
+		}
+
+		Delay(1000);
 
 		time_var2 = 0;
 		for (int i = 0;i < 1000000;i++) {
@@ -59,17 +83,21 @@ void calculation_test() {
 		iteration++;
 	}
 }
+*/
 
 void init() {
 	GPIO_InitTypeDef  GPIO_InitStructure;
 	USART_InitTypeDef USART_InitStructure;
-	DAC_InitTypeDef  DAC_InitStructure;
+//	DAC_InitTypeDef  DAC_InitStructure;
 
 	// ---------- SysTick timer -------- //
-	if (SysTick_Config(SystemCoreClock / 1000)) {
+	//if (SysTick_Config(SystemCoreClock / 1000)) {
 		// Capture error
-		while (1){};
-	}
+	//	while (1){};
+	//}
+
+	INTTIM_Config();
+	Configure_PA0();
 
 	// GPIOD Periph clock enable
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
@@ -120,10 +148,10 @@ void init() {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 	// Configuration
-	DAC_InitStructure.DAC_Trigger = DAC_Trigger_None;
-	DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
-	DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Disable;
-	DAC_Init(DAC_Channel_1, &DAC_InitStructure);
+	//DAC_InitStructure.DAC_Trigger = DAC_Trigger_None;
+	//DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
+	//DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Disable;
+	//DAC_Init(DAC_Channel_1, &DAC_InitStructure);
 
 	// IO
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
@@ -132,35 +160,163 @@ void init() {
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	// Enable DAC Channel1
-	DAC_Cmd(DAC_Channel_1, ENABLE);
+	//DAC_Cmd(DAC_Channel_1, ENABLE);
 
 	// Set DAC Channel1 DHR12L register
-	DAC_SetChannel1Data(DAC_Align_12b_R, 0);
+	//DAC_SetChannel1Data(DAC_Align_12b_R, 0);
 }
 
 /*
  * Called from systick handler
- */
+ *
 void timing_handler() {
+	int value = (time_var2/5000) % 4;
+	switch (value) {
+		case 0:
+			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+			break;
+		case 1:
+			GPIO_SetBits(GPIOD, GPIO_Pin_12);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+			break;
+		case 2:
+			GPIO_SetBits(GPIOD, GPIO_Pin_14);
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+			break;
+		case 3:
+			GPIO_SetBits(GPIOD, GPIO_Pin_14);
+			GPIO_SetBits(GPIOD, GPIO_Pin_12);
+			break;
+	}
 	if (time_var1) {
 		time_var1--;
 	}
 
 	time_var2++;
 }
+*/
 
-/*
- * Delay a number of systick cycles (1ms)
- */
-void Delay(volatile uint32_t nCount) {
-	time_var1 = nCount;
-	while(time_var1){};
+/* Configure pins to be interrupts */
+void Configure_PA0(void) {
+    /* Set variables used */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+    
+    /* Enable clock for GPIOD */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    /* Enable clock for SYSCFG */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    
+    /* Set pin as input */
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
+    
+    /* Tell system that you will use PD0 for EXTI_Line0 */
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+    
+    /* PD0 is connected to EXTI_Line0 */
+    EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+    /* Enable interrupt */
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    /* Interrupt mode */
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    /* Triggers on rising and falling edge */
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+    /* Add to EXTI */
+    EXTI_Init(&EXTI_InitStruct);
+ 
+    /* Add IRQ vector to NVIC */
+    /* PD0 is connected to EXTI_Line0, which has EXTI0_IRQn vector */
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+    /* Set priority */
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+    /* Set sub priority */
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+    /* Enable interrupt */
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    /* Add to NVIC */
+    NVIC_Init(&NVIC_InitStruct);
 }
+
+/* Set interrupt handlers */
+/* Handle PD0 interrupt */
+void EXTI0_IRQHandler(void) {
+    /* Make sure that interrupt flag is set */
+    if (EXTI_GetITStatus(EXTI_Line0) != RESET) {
+		if (debounce == 0) {
+			incrementCounter();
+			debounce = 500;
+		}
+        
+        /* Clear interrupt flag */
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}
+
+void incrementCounter(void)
+{
+	int LED_MASK = 0b1111 << 12;
+	GPIOD->ODR += (1 << 12);
+	if(GPIOD->ODR & LED_MASK == 0b1111) 
+		GPIOD->ODR ^= 0b1111 << 12;
+}
+
+
+void TIM2_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) {
+		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+		if (time_var1) {
+			time_var1--;
+		}
+		time_var2++;
+		if (debounce) {
+			debounce--;
+		}
+		if (time_var2 % 100 == 0) {
+            //Do stuff
+		}
+	}
+}
+
+void INTTIM_Config(void) {
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* Enable the TIM2 gloabal Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+ 
+	/* TIM2 clock enable */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+	/* Time base configuration */
+	TIM_TimeBaseStructure.TIM_Period = 1000 - 1; // 1 MHz down to 1 KHz (1 ms)
+	TIM_TimeBaseStructure.TIM_Prescaler = 84 - 1; // 24 MHz Clock down to 1 MHz (adjust per your clock)
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+	/* TIM IT enable */
+	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+	
+	/* TIM2 enable counter */
+	TIM_Cmd(TIM2, ENABLE);
+}
+
 
 /*
  * Dummy function to avoid compiler error
  */
 void _init() {
-
 }
 
