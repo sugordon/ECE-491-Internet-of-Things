8d5da8a73c4c797f26fae0fb3cd6fadf9ae6ae99
Gordon Su <gordonsu14@gmail.com>
2016-10-26 17:21:17 -0400

    Redirect clock to gpio

--- src/main.c
+++ src/main.c
@@ -181,20 +181,43 @@ void SendData(USART_TypeDef* USARTx, volatile char *s){
     }
 }
 
-// ---------------------------- //
-// ---------- GPIOD ----------- //
-// ---------------------------- //
+// --------------------------- //
+// ---------- GPIO ----------- //
+// --------------------------- //
 
-/* Configure GPIOD output pins for LEDs */
+/* Configure GPIO pins */
 void InitGPIOD(void) {
 
     GPIO_InitTypeDef GPIO_InitStructure;
+
+    /* Configure GPIOD output pins for LEDs */
 
     // GPIOD Periph clock enable
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
     // Configure PD12, PD13, PD14 and PD15 in output pushpull mode
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    /* Configure output pins for MCO1, MCO2 */
+    
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+    // Configure PA8 in output mode
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    // Configure PC9 in output mode
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
